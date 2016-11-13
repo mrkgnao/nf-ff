d6a5c488cca4ad5ca222de31eef4e3cf6b8df9be
Soham Chowdhury <chow.soham@gmail.com>
2016-11-13 19:37:04 +0530

    Get started

---
+++ chapters/auto/artin-whaples.el
@@ -0,0 +1,44 @@
+(TeX-add-style-hook
+ "artin-whaples"
+ (lambda ()
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
+   (LaTeX-add-labels
+    "sec:orgheadline1"
+    "sec:orgheadline6"
+    "sec:orgheadline2"
+    "sec:orgheadline3"
+    "sec:orgheadline4"
+    "sec:orgheadline5"
+    "sec:orgheadline26"
+    "sec:orgheadline7"
+    "sec:orgheadline19"
+    "sec:orgheadline8"
+    "sec:orgheadline9"
+    "sec:orgheadline12"
+    "sec:orgheadline10"
+    "sec:orgheadline11"
+    "sec:orgheadline16"
+    "sec:orgheadline13"
+    "sec:orgheadline15"
+    "sec:orgheadline14"
+    "sec:orgheadline17"
+    "sec:orgheadline18"
+    "sec:orgheadline20"
+    "sec:orgheadline21"
+    "sec:orgheadline25"
+    "sec:orgheadline22"
+    "sec:orgheadline23"
+    "sec:orgheadline24"
+    "sec:orgheadline28"
+    "sec:orgheadline27"
+    "sec:orgheadline31"
+    "sec:orgheadline29"
+    "sec:orgheadline30"))
+ :latex)
+
---
+++ chapters/auto/frenkel.el
@@ -0,0 +1,7 @@
+(TeX-add-style-hook
+ "frenkel"
+ (lambda ()
+   (LaTeX-add-labels
+    "sec:adeles-ideles"))
+ :latex)
+
